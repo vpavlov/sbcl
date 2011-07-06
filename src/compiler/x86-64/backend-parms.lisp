30818523cc0185c2dfd9c70d0d6e46224d66cab3
Paul Khuong <pvk@pvk.ca>
2011-07-06 00:15:27 -0400

    Tweak constants

--- src/compiler/x86-64/backend-parms.lisp
+++ src/compiler/x86-64/backend-parms.lisp
@@ -47,7 +47,7 @@
 ;;; The size in bytes of GENCGC cards, i.e. the granularity at which
 ;;; writes to old generations are logged.  With mprotect-based write
 ;;; barriers, this must be a multiple of the OS page size.
-(def!constant gencgc-card-bytes *backend-page-bytes*)
+(def!constant gencgc-card-bytes 4096)
 ;;; The minimum size of new allocation regions.  While it doesn't
 ;;; currently make a lot of sense to have a card size lower than
 ;;; the alloc granularity, it will, once we are smarter about finding
@@ -55,4 +55,4 @@
 (def!constant gencgc-alloc-granularity 0)
 ;;; The minimum size at which we release address ranges to the OS.
 ;;; This must be a multiple of the OS page size.
-(def!constant gencgc-release-granularity *backend-page-bytes*)
+(def!constant gencgc-release-granularity (ash 1 21))
